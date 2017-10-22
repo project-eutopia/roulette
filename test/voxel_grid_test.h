dfe9e6028e2e50eb78fdb81ae8e12a824fbceff4
Christopher Locke <project.eutopia@gmail.com>
2017-10-22 15:29:51 -0700

    Move ray trace tests to VoxelGridTest

--- test/voxel_grid_test.h
+++ test/voxel_grid_test.h
@@ -209,3 +209,237 @@ TEST(VoxelGridTest, transport_beamlet_particle_to_surface) {
   bool res = grid.transport_particle_to_surface(&current_photon);
   ASSERT_TRUE(res);
 }
+
+TEST(VoxelGridTest, ray_trace_voxels_test_x_dir) {
+  ThreeVector position(-10, 0, 0);
+  ThreeVector velocity(1, 0, 0);
+
+  std::shared_ptr<const VoxelGrid> voxel_grid = std::make_shared<const VoxelGrid>(
+    ThreeVector(-5, -5, -5),
+    ThreeVector( 5,  5,  5),
+    4,4,2
+  );
+
+  std::vector<std::vector<int>> expected_voxels = {
+    {0, 2, 1},
+    {1, 2, 1},
+    {2, 2, 1},
+    {3, 2, 1}
+  };
+  int i = 0;
+
+  ThreeVector final_position = voxel_grid->ray_trace_voxels(
+    position, velocity,
+    IVoxelGrid::voxel_iterator(
+      [&](double distance, size_t xi, size_t yi, size_t zi) -> double {
+        EXPECT_EQ(xi, expected_voxels[i][0]);
+        EXPECT_EQ(yi, expected_voxels[i][1]);
+        EXPECT_EQ(zi, expected_voxels[i][2]);
+        EXPECT_EQ(distance, 2.5);
+        i++;
+
+        return distance;
+      }
+    )
+  );
+
+  EXPECT_EQ(i, 4);
+  EXPECT_EQ(final_position(0), 5);
+  EXPECT_EQ(final_position(1), 0);
+  EXPECT_EQ(final_position(2), 0);
+}
+
+TEST(VoxelGridTest, ray_trace_voxels_test_y_dir) {
+  ThreeVector position(0, -10, 0);
+  ThreeVector velocity(0, 1, 0);
+
+  std::shared_ptr<const VoxelGrid> voxel_grid = std::make_shared<const VoxelGrid>(
+    ThreeVector(-5, -5, -5),
+    ThreeVector( 5,  5,  5),
+    4,4,2
+  );
+
+  std::vector<std::vector<int>> expected_voxels = {
+    {2, 0, 1},
+    {2, 1, 1},
+    {2, 2, 1},
+    {2, 3, 1}
+  };
+  int i = 0;
+
+  ThreeVector final_position = voxel_grid->ray_trace_voxels(
+    position, velocity,
+    IVoxelGrid::voxel_iterator(
+      [&](double distance, int xi, int yi, int zi) -> double {
+        EXPECT_EQ(xi, expected_voxels[i][0]);
+        EXPECT_EQ(yi, expected_voxels[i][1]);
+        EXPECT_EQ(zi, expected_voxels[i][2]);
+        EXPECT_EQ(distance, 2.5);
+        i++;
+
+        return distance;
+      }
+    )
+  );
+
+  EXPECT_EQ(i, 4);
+  EXPECT_EQ(final_position(0), 0);
+  EXPECT_EQ(final_position(1), 5);
+  EXPECT_EQ(final_position(2), 0);
+}
+
+TEST(VoxelGridTest, ray_trace_voxels_test_z_dir) {
+  ThreeVector position(0, 0, 10);
+  ThreeVector velocity(0, 0, -1);
+
+  std::shared_ptr<const VoxelGrid> voxel_grid = std::make_shared<const VoxelGrid>(
+    ThreeVector(-5, -5, -5),
+    ThreeVector( 5,  5,  5),
+    4,4,2
+  );
+
+  std::vector<std::vector<int>> expected_voxels = {
+    {2, 2, 1},
+    {2, 2, 0}
+  };
+  int i = 0;
+
+  ThreeVector final_position = voxel_grid->ray_trace_voxels(
+    position, velocity,
+    IVoxelGrid::voxel_iterator(
+      [&](double distance, size_t xi, size_t yi, size_t zi) -> double {
+        EXPECT_EQ(xi, expected_voxels[i][0]);
+        EXPECT_EQ(yi, expected_voxels[i][1]);
+        EXPECT_EQ(zi, expected_voxels[i][2]);
+        EXPECT_EQ(distance, 5);
+        i++;
+
+        return distance;
+      }
+    )
+  );
+
+  EXPECT_EQ(i, 2);
+  EXPECT_EQ(final_position(0), 0);
+  EXPECT_EQ(final_position(1), 0);
+  EXPECT_EQ(final_position(2),-5);
+}
+
+TEST(VoxelGridTest, ray_trace_voxels_test_going_through_edge) {
+  ThreeVector position(10, -5, 0);
+  ThreeVector velocity(-2, 1, 0);
+
+  std::shared_ptr<const VoxelGrid> voxel_grid = std::make_shared<const VoxelGrid>(
+    ThreeVector(-5, -5, -5),
+    ThreeVector( 5,  5,  5),
+    4,4,2
+  );
+
+  std::vector<std::vector<int>> expected_voxels = {
+    {3, 1, 1},
+    {2, 1, 1},
+    {1, 2, 1},
+    {0, 2, 1}
+  };
+  int i = 0;
+
+  ThreeVector final_position = voxel_grid->ray_trace_voxels(
+    position, velocity,
+    IVoxelGrid::voxel_iterator(
+      [&](double distance, size_t xi, size_t yi, size_t zi) -> double {
+        EXPECT_EQ(xi, expected_voxels[i][0]);
+        EXPECT_EQ(yi, expected_voxels[i][1]);
+        EXPECT_EQ(zi, expected_voxels[i][2]);
+        EXPECT_EQ(distance, std::sqrt(2.5*2.5 + 1.25*1.25));
+        i++;
+
+        return distance;
+      }
+    )
+  );
+
+  EXPECT_EQ(i, 4);
+  EXPECT_EQ(final_position(0),  -5);
+  EXPECT_EQ(final_position(1), 2.5);
+  EXPECT_EQ(final_position(2),   0);
+}
+
+TEST(VoxelGridTest, ray_trace_voxels_test_going_through_corner) {
+  ThreeVector position(-3, 3, -3);
+  ThreeVector velocity(1, -1, 1);
+
+  std::shared_ptr<const VoxelGrid> voxel_grid = std::make_shared<const VoxelGrid>(
+    ThreeVector(-5, -5, -5),
+    ThreeVector( 5,  5,  5),
+    4,4,2
+  );
+
+  std::vector<std::vector<int>> expected_voxels = {
+    {0, 3, 0},
+    {1, 2, 0},
+    {2, 1, 1},
+    {3, 0, 1}
+  };
+  int i = 0;
+
+  double distance_1 = std::sqrt(3*0.5*0.5);
+  double distance_2 = std::sqrt(3*2.5*2.5);
+
+  ThreeVector final_position = voxel_grid->ray_trace_voxels(
+    position, velocity,
+    IVoxelGrid::voxel_iterator(
+      [&](double distance, size_t xi, size_t yi, size_t zi) -> double {
+        EXPECT_EQ(xi, expected_voxels[i][0]);
+        EXPECT_EQ(yi, expected_voxels[i][1]);
+        EXPECT_EQ(zi, expected_voxels[i][2]);
+        if (i == 0) {
+          EXPECT_NEAR(distance, distance_1, 0.0000001);
+        }
+        else {
+          EXPECT_NEAR(distance, distance_2, 0.0000001);
+        }
+        i++;
+
+        return distance;
+      }
+    )
+  );
+
+  EXPECT_EQ(i, 4);
+  EXPECT_EQ(final_position(0),  5);
+  EXPECT_EQ(final_position(1), -5);
+  EXPECT_EQ(final_position(2),  5);
+}
+
+TEST(VoxelGridTest, ray_trace_voxels_going_through_random_directions) {
+  std::shared_ptr<const VoxelGrid> voxel_grid = std::make_shared<const VoxelGrid>(
+    ThreeVector(0, -10, -10),
+    ThreeVector(10, 10, 10),
+    256,1,1
+  );
+
+  ThreeVector start_position(-5, 0, 0);
+  RandomGenerator generator;
+
+  for (int i = 0; i < 5; ++i) {
+    double theta = M_PI/4 * (generator.uniform()-0.5);
+    double phi = 4*M_PI * (generator.uniform()-0.5);
+    ThreeVector velocity(std::cos(theta), std::sin(theta)*std::cos(phi), std::sin(theta)*std::cos(phi));
+
+    int count = 0;
+    ThreeVector final_position = voxel_grid->ray_trace_voxels(
+      start_position, velocity,
+      IVoxelGrid::voxel_iterator(
+        [&](double distance, size_t xi, size_t yi, size_t zi) -> double {
+          EXPECT_EQ(xi, count);
+          ++count;
+
+          return distance;
+        }
+      )
+    );
+
+    EXPECT_EQ(count, 256);
+    EXPECT_NEAR(final_position(0), 10, 0.0000001);
+  }
+}
